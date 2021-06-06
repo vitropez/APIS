868c08465e0eaf41f25c2a9db22bf64fe106c43c
vitropez <64587299+vitropez@users.noreply.github.com>
2021-06-06 12:54:29 +0200

    audio

--- plane.js
+++ plane.js
@@ -1,50 +1,50 @@
 function iniciar() {
     maximo=600;
     medio=document.getElementById('medio');
-      reproducir=document.getElementById('reproducir');
-    barra=document.getElementById('barra');
+    reproducir=document.getElementById('reproducir');
     progreso=document.getElementById('progreso');
-    reproducir.addEventListener('click', presionar, false);
-
-    barra.addEventListener('click', mover, false);
+    reproducir.addEventListener('click', doblePlay, false);
+    audio=document.getElementById('audio');
 }
-function presionar(){
+function doblePlay(){
+    presionarVideo();
+    presionarAudio();
+}
+function presionarVideo(){
     if(!medio.paused && !medio.ended) {
         medio.pause();
-
-
         reproducir.innerHTML='Reproducir';
         window.clearInterval(bucle);
 
     }else{
         medio.play();
-
-
         reproducir.innerHTML='Pausa';
         bucle=setInterval(estado, 700);
 
     }
 }
+function presionarAudio(){
+    if(!audio.paused && !audio.ended) {
+        audio.pause();
+    }else{
+        audio.play();
 
-
-    function estado() {
-        if (!medio.ended) {
+   }
+}
+function estado() {
+       if (!medio.ended) {
             var total = parseInt(medio.currentTime * maximo / medio.duration);
-
-
-
             progreso.innerHTML += ("&#9992;&#65039;");
-console.log(total)
-            if (total>550) {
-                progreso.innerHTML = ("");
+
+           if (total>550) {
+            progreso.innerHTML = ("");
+           }
+       }else
+            {
+               progreso.style.width = '0px';
+                reproducir.innerHTML = 'Reproducir';
+               window.clearInterval(bucle);
             }
-
-        } else {
-            progreso.style.width = '0px';
-
-            reproducir.innerHTML = 'Reproducir';
-            window.clearInterval(bucle);
-        }
 
     }
 
