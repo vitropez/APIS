ae91244505b6e417f5bc969e15136f262124325a
vitropez <64587299+vitropez@users.noreply.github.com>
2021-06-06 18:37:13 +0200

    ejericio para entrega

--- plane.js
+++ plane.js
@@ -1,3 +1,5 @@
+//creamos la función iniciar que se ejecutará al cargar la página y
+//guardamos los identificadores que vamos a utilizar en una variable
 function iniciar() {
     maximo=600;
     medio=document.getElementById('medio');
@@ -6,16 +8,20 @@ function iniciar() {
     reproducir.addEventListener('click', doblePlay, false);
     audio=document.getElementById('audio');
 }
+//al hacer click en el botón se ejecuta la función doblePlay que lleva
+//asociadas la función de encender el video y el audio
 function doblePlay(){
     presionarVideo();
     presionarAudio();
 }
+//si el video no está pausado o finalizado se pausará al presionarlo y cambiará
+//el texto a reproducir
 function presionarVideo(){
     if(!medio.paused && !medio.ended) {
         medio.pause();
         reproducir.innerHTML='Reproducir';
         window.clearInterval(bucle);
-
+//si el video está pausado se ejecutará en un intervalo de 0,7 segundos
     }else{
         medio.play();
         reproducir.innerHTML='Pausa';
@@ -23,29 +29,33 @@ function presionarVideo(){
 
     }
 }
+//igual para la función de audio
 function presionarAudio(){
     if(!audio.paused && !audio.ended) {
         audio.pause();
-    }else{
+    }
+    else{
         audio.play();
-
    }
 }
+//está función es la que se ejecuta cada 0,7 segundos
+//calculamos el tiempo de duración del video nunca superior a 600 y
+//escribimos los aviones en la barra de reprodución hasta que finalice el
+//video que se borrarán los aviones
+//el video y el audio están en loop por lo que no se detienen a menos que se
+//paren con el botón
 function estado() {
        if (!medio.ended) {
             var total = parseInt(medio.currentTime * maximo / medio.duration);
-            progreso.innerHTML += ("&#9992;&#65039;");
+            progreso.innerHTML += ("&#9992;&#65039;&#9992;&#65039;o");
 
-           if (total>550) {
+           if (total>560) {
             progreso.innerHTML = ("");
            }
        }else
             {
-               progreso.style.width = '0px';
                 reproducir.innerHTML = 'Reproducir';
-               window.clearInterval(bucle);
             }
-
     }
-
+//la función iniciar se ejecutará cuando cargue la página
 window.addEventListener('load', iniciar, false);
